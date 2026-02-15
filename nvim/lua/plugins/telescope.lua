8f190d2fc1ae874994b34f16fc12876cfc16c28c
Arman Hosseini <armanhosseini878787@gmail.com>
2026-02-15 09:57:00 +0000

    change rg to respect .gitignore even outside a git repo

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -13,7 +13,11 @@ return {
       local builtin = require('telescope.builtin')
 
       vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+      vim.keymap.set("n", "<leader>ff",
+        function()
+          builtin.find_files({find_command = {"rg", "--files", "--hidden", "--no-require-git"}}
+        end,
+        { desc = "Telescope find files" })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
     end
