5e800eea6bf72ca384b6806ac79e5182dbe12dc8
Arman Hosseini <armanhosseini878787@gmail.com>
2026-02-16 15:56:21 +0000

    add nvim-ros2

---
+++ nvim/lua/plugins/nvim-ros2.lua
@@ -0,0 +1,45 @@
+return {
+  "ErickKramer/nvim-ros2",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  lazy = false,
+  opts = {
+    autocmds = true,
+    treesitter = true,
+  },
+  keys = {
+    { "<leader>li", function() require("nvim-ros2").pickers.interfaces() end, desc = "[ROS 2]: List interfaces" },
+    { "<leader>ln", function() require("nvim-ros2").pickers.nodes() end, desc = "[ROS 2]: List nodes" },
+    { "<leader>la", function() require("nvim-ros2").pickers.actions() end, desc = "[ROS 2]: List actions" },
+    { "<leader>lt", function() require("nvim-ros2").pickers.topics_info() end, desc = "[ROS 2]: List topics with info" },
+    { "<leader>le", function() require("nvim-ros2").pickers.topics_echo() end, desc = "[ROS 2]: List topics with echo" },
+    { "<leader>ls", function() require("nvim-ros2").pickers.services() end, desc = "[ROS 2]: List services" },
+  },
+  config = function()
+    print("bluh bluh")
+
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+    vim.filetype.add({
+      extension = {
+        msg = "ros2",
+        srv = "ros2",
+        action = "ros2",
+      },
+    })
+
+    parser_config.ros2 = {
+      install_info = {
+        url = "~/.local/share/nvim/lazy/nvim-ros2/treesitter-ros2",
+        files = {"src/parser.c"},
+        branch = "main",
+        generate_requires_npm = false,
+        requires_generate_from_grammar = false,
+      },
+      filetype = "ros2",
+    }
+  end,
+}
