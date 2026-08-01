e247341c4f2ee10c4b559b8468eae242560f163c
Arman Hosseini <armanhosseini878787@gmail.com>
2026-08-01 12:03:49 +0330

    update theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,8 +1,18 @@
-return { 
-  "catppuccin/nvim", 
-  name = "catppuccin", 
-  priority = 1000, 
-	config = function()
-		vim.cmd.colorscheme("catppuccin-mocha")
-	end,
+-- return {
+--   "catppuccin/nvim",
+--   name = "catppuccin",
+--   priority = 1000,
+-- 	config = function()
+-- 		vim.cmd.colorscheme("catppuccin-mocha")
+-- 	end,
+-- }
+
+return {
+  "nkxxll/ghostty-default-style-dark.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("ghostty-default-style-dark").setup({})
+    vim.cmd.colorscheme("ghostty-default-style-dark")
+  end
 }
