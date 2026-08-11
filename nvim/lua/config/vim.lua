0d27126728dc1611ee8092f742dbac52d8ff4eff
Arman Hosseini <armanhosseini878787@gmail.com>
2026-08-11 17:07:58 +0330

    remove some shortcuts in order to respect the defaults

--- nvim/lua/config/vim.lua
+++ nvim/lua/config/vim.lua
@@ -23,19 +23,6 @@ vim.o.showbreak = "↪ "
 
 vim.o.shell = os.getenv("SHELL")
 
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', {
-  desc = 'Move focus to the left window'
-})
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', {
-  desc = 'Move focus to the right window'
-})
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', {
-  desc = 'Move focus to the lower window'
-})
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', {
-  desc = 'Move focus to the upper window'
-})
-
 vim.keymap.set('n', '<C-e>', ':Ex<CR>', { desc = 'Open netrw' })
 
 vim.keymap.set('n', '<leader>o', 'o<Esc>O', { desc = 'Double new line' })
